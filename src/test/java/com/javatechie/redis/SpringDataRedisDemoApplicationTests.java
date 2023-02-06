f63f598cb596f534c30f7a41d62be89c200577df
ashishrawat732 <ashishrawat732@gmail.com>
2023-02-06 17:00:45 +0530

    spring data redis with jenkins

--- src/test/java/com/javatechie/redis/SpringDataRedisDemoApplicationTests.java
+++ src/test/java/com/javatechie/redis/SpringDataRedisDemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.javatechie.redis;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringDataRedisDemoApplicationTests {
 
+	public static Logger Logger = LoggerFactory.getLogger(SpringDataRedisDemoApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		Logger.info("Test case executing...");
+		Logger.info("Test case executing second log statement...");
+		assertEquals(true,true);
 	}
 
 }
