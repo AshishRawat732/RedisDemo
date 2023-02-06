91f7bcddea607bb1e1984d06810b33c90d5ddd54
ashishrawat732 <ashishrawat732@gmail.com>
2023-02-06 17:46:23 +0530

    spring data redis with Jenkins 2

--- src/main/java/com/javatechie/redis/SpringDataRedisDemoApplication.java
+++ src/main/java/com/javatechie/redis/SpringDataRedisDemoApplication.java
@@ -1,13 +1,25 @@
 package com.javatechie.redis;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDataRedisDemoApplication {
 
+	public static Logger Logger = LoggerFactory.getLogger(SpringDataRedisDemoApplication.class);
+
+   @PostConstruct
+   public void init(){
+	   Logger.info("Application started");
+   }
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataRedisDemoApplication.class, args);
+		Logger.info("Application executed");
+	   SpringApplication.run(SpringDataRedisDemoApplication.class, args);
 	}
 
 }
